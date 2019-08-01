a3f6d7a2b5231a1264de4677fac548bb772c9e4f
eff-im <efim-bukin@yandex.ru>
2019-08-01 23:56:37 +0700

    add code

--- python_2.py
+++ python_2.py
@@ -2,17 +2,19 @@
 def odometer(N):
     V = 1
     t = 1
+    itog = 0
     for i in range(len(N)):
         if i % 2 == 0:
             V = N[i]
             #print('V -', V)
-        else:
-            t = N[i]
+        elif i % 2 != 0 and i != 0:
+            t = N[i] - 1
             #print('t -', t)
         S = V * t
+        itog += S 
         #print('S -', S)
 
-    return S
+    return itog
 
-#print(odometer([10,1,20,2,30,3]))
+#print(odometer([10, 1, 20, 2, 30, 3]))
     
