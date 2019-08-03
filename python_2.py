b063fac4f03f87109a0a4ea2dcd60cc9b645df8f
eff-im <efim-bukin@yandex.ru>
2019-08-03 23:28:53 +0700

    Переосмысление алгаритма

--- python_2.py
+++ python_2.py
@@ -1,17 +1,11 @@
 # Задание 2
 def odometer(N):
-    V = 0
     S = 0
-    t = 0
     for i in range(len(N)):
-        if i % 2 == 0:
-            V = V + N[i]
-            #print(i + 1, '- итерация, V (Скорость) -', V)
-        elif i % 2 != 0 and i != 0:
-            t = N[i]
-            #print(i + 1, '- итерация, t (Время) -', t)
-        #print('')
-    S = V * t
+        if i % 2 == 0 and i != 0:
+            S = S + (N[i] * N[i + 1])
+            #print(S)
+    S = S + (N[0] * N[1])
     #print('S (Расстояние) -', S)
     return S
 
