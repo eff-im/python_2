4ef491b6c1637c780c53141765b80aa23466d8c6
eff-im <efim-bukin@yandex.ru>
2019-08-03 23:58:47 +0700

    Еще одно переосиысление решения

--- python_2.py
+++ python_2.py
@@ -1,14 +1,12 @@
 # Задание 2
 def odometer(N):
+    V = 0
     S = 0
+    t2 = 0
     for i in range(len(N)):
-        if i % 2 == 0 and i != 0:
-            S = S + (N[i] * N[i + 1])
-            #print(S)
-    S = S + (N[0] * N[1])
-    #print('S (Расстояние) -', S)
+        if i % 2 == 0:
+            V = N[i]
+            t = N[i + 1] - t2
+            t2 = N[i + 1]
+            S = S + V * t
     return S
-
-#print(odometer([20,2,30,6,10,7]))
-
-    
